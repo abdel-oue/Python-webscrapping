cf748263e68a53a4e866edac800496a0dc09abeb
abdel-oue <dryalapah@gmail.com>
2025-10-25 15:55:56 +0100

    Initial

---
+++ web-scrapping.py
@@ -0,0 +1,14 @@
+"""
+beautifulsoup for web scraping
+"""
+
+import os,sys,re
+from os import path
+def extract(url :str) ->list[tuple[str,...]]:
+    results=[]
+    
+    
+    #(year, 1st male name, 1st female name)
+    #check if url is dir
+    # iter of dir to search for expected data
+    return results
