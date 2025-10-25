4962b3ee4c27076dbd6fd1e2798520b398588f95
abdel-oue <dryalapah@gmail.com>
2025-10-25 17:16:50 +0100

    functions extract_popularity_year extract_babies from kricha

--- web-scrapping.py
+++ web-scrapping.py
@@ -2,29 +2,45 @@ import os,sys,re
 from os import path
 from bs4 import BeautifulSoup # beautifulsoup for web scraping
 
-def extract(url :str) ->list[tuple[str,...]]: # we want to return a list 
-    try :
-        with open(url,'r') as htmlfile: # closes when block is finished
-            content = htmlfile.read()
+def extract_babies(url :str) ->list[tuple[str,...]]: # we want to return a list of tuple (rank , male name, female name)
+    with open(url,'r') as htmlfile:
+        all_names = []
+        content = htmlfile.read()
+        soup = BeautifulSoup(content, 'lxml') # as string
+        baby_names = soup.find_all('tr', align='right')
+        for column in baby_names:
+            cleaned_row = column.find_all('td')
+            if len(cleaned_row) == 3:
+                cell1, cell2, cell3 = cleaned_row
+                rank = cell1.text
+                male_name = cell2.text
+                female_name = cell3.text
+                all_names.append((rank,male_name,female_name))
+        return all_names
 
-            soup = BeautifulSoup(content, 'lxml') # as string
-            baby_names = soup.find_all('td')
+def extract_popularity_year(url :str) -> str:
+    with open(url,'r') as htmlfile:
+        content = htmlfile.read()
+        soup = BeautifulSoup(content, 'lxml') # as string
+        popularity_years_info = soup.find_all('h3', align='center')
+        popularity_string = popularity_years_info[0].text
+        popularity_year = popularity_string.replace("Popularity in ","")
+        return popularity_year
 
-            print(baby_names)
-            
-            
+
+def extract_info(url: str) -> tuple[list[tuple[str, ...]], str]:
+    try:
+        all_names = extract_babies(url)
+        popularity_year = extract_popularity_year(url)
+        return all_names, popularity_year
     except FileNotFoundError:
         print(f'{url} not found')
-        return []
+        return [], ''
     except PermissionError:
         print(f'{url} permission denied')
-        return []
-    
-    #(year, 1st male name, 1st female name)
-    #check if url is dir
-    # iter of dir to search for expected data
-    results=[]
-    return results
+        return [], ''
+
+
 
 if __name__ == '__main__':
-    extract('./data/baby1990.html')
+    extract_popularity_year('./data/baby1990.html')
